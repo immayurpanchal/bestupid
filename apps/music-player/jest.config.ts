f925b63c3694b629f18ba40f1420a279693c755b
Mayur Panchal <mayur.panchal@deepintent.com>
2022-08-16 15:17:47 +0530

    initial commit

---
+++ apps/music-player/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'music-player',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/music-player',
+};
