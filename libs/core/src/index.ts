c58b3bc1bcdd660f09cdc99b894a906ff0e14c78
Mayur Panchal <mayur.panchal@deepintent.com>
2022-08-17 15:30:09 +0530

    feat(lib): :sparkles: add checkbox comp

--- libs/core/src/index.ts
+++ libs/core/src/index.ts
@@ -1,2 +1,3 @@
+export * from './lib/checkbox/checkbox'
 export * from './lib/icons'
 export * from './lib/button/button'
---
+++ libs/core/src/lib/checkbox/checkbox.tsx
@@ -0,0 +1,58 @@
+import classNames from 'classnames'
+import React, { useRef } from 'react'
+import Tick from '../icons/Tick'
+
+type CheckboxProps = {
+	checked?: boolean
+	onChange?: (checked: boolean) => void
+	widthClassName?: React.SVGAttributes<SVGSVGElement>['className']
+	heightClassName?: React.SVGAttributes<SVGSVGElement>['className']
+	fillClassName?: React.SVGAttributes<SVGSVGElement>['className']
+	text: string
+}
+
+export const Checkbox = (props: CheckboxProps) => {
+	const { checked = false, widthClassName = '', heightClassName = '', fillClassName, onChange, text } = props
+
+	const inputRef = useRef<HTMLInputElement>(null)
+
+	const containerClassName = classNames(
+		'bg-grey-50 rounded-lg relative p-1 focus-visible:outline focus-visible:outline-1 focus-visible:outline-dark-100 focus-visible:outline-offset-1',
+		{
+			[widthClassName]: widthClassName,
+			[heightClassName]: heightClassName,
+			'neumorphism-pressed': checked,
+			neumorphism: !checked
+		}
+	)
+
+	const handleClick = () => {
+		if (inputRef.current) {
+			inputRef.current.checked = !inputRef.current.checked
+			onChange && onChange(inputRef.current.checked)
+		}
+	}
+
+	const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+		if (e.key === 'Enter' || e.key === ' ') {
+			handleClick()
+		}
+	}
+
+	return (
+		<label
+			className='relative flex select-none items-center gap-x-2 text-base'
+			htmlFor='checkbox-custom'
+			id='checkbox'
+			onClick={handleClick}
+		>
+			<div className={containerClassName} tabIndex={0} onKeyDown={handleKeyDown}>
+				{checked && <Tick fillClassName={fillClassName} />}
+			</div>
+			{text}
+			<input ref={inputRef} className='absolute hidden' name='checkbox-custom' type='checkbox' />
+		</label>
+	)
+}
+
+export default Checkbox
