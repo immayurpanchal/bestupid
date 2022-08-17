d6f2479d55625a1426f88e85cb20e29d2823403e
Mayur Panchal <mayur.panchal@deepintent.com>
2022-08-17 11:06:32 +0530

    chore: :wrench: add music player tailwind theme
    update tailwind.config

--- apps/music-player/tailwind.config.js
+++ apps/music-player/tailwind.config.js
@@ -8,7 +8,28 @@ module.exports = {
 		...createGlobPatternsForDependencies(__dirname)
 	],
 	theme: {
-		extend: {}
+		colors: {
+			white: '#fff',
+			dark: {
+				100: '#6D81A1',
+				900: '#041F1E'
+			},
+			skin: { 100: '#F9B9B0', 500: '#DE958B' },
+			grey: {
+				50: '#E9F3FB',
+				100: '#E4F0FA'
+			}
+		},
+		extend: {
+			boxShadow: {
+				lg: '-2px -2px 4px rgba(255, 255, 255, 0.5), 2px 2px 4px rgba(109, 129, 161, 0.25)',
+				inset: 'inset -4px -4px 6px rgba(255, 255, 255, 0.5), inset 4px 4px 6px rgba(109, 129, 161, 0.16)',
+				'slider-track': 'inset 0px 4px 4px rgba(4, 31, 30, 0.25)'
+			},
+			borderRadius: {
+				'4xl': '2rem'
+			}
+		}
 	},
 	plugins: []
 }
