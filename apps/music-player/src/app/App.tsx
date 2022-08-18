bf711f18110085fd0cf4edc7d6e03c9441d55dda
Mayur Panchal <mayur.panchal@deepintent.com>
2022-08-18 09:54:50 +0530

    feat: :sparkles: add search page

--- apps/music-player/src/app/App.tsx
+++ apps/music-player/src/app/App.tsx
@@ -3,11 +3,13 @@ import Details from './pages/Details'
 import Home from './pages/Home'
 import NotFound from './pages/NotFound'
 import Player from './pages/Player'
+import Search from './pages/Search'
 
 export const App = () => {
 	return (
 		<BrowserRouter>
 			<Routes>
+				<Route element={<Search />} path='/search' />
 				<Route element={<Home />} path='/' />
 				<Route element={<Player />} path='/player' />
 				<Route element={<Details />} path='/details' />
---
+++ apps/music-player/src/app/pages/Search.tsx
@@ -0,0 +1,103 @@
+import { BackChevron, Button, Keyboard as KeyboardIcon, SearchIcon } from '@bestupid/core'
+import { useRef, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import useSWR from 'swr'
+
+type Image = {
+	link: string
+}
+
+type DownloadUrl = {
+	quality: string
+	link: string
+}
+
+type Song = {
+	id: string
+	image: Array<Image>
+	name: string
+	year: number
+	downloadUrl: Array<DownloadUrl>
+}
+
+const fetcher = (url: string) => fetch(url).then(r => r.json())
+
+const SearchPage = () => {
+	const inputRef = useRef<HTMLInputElement>(null)
+	const navigate = useNavigate()
+	const [searchValue, setSearchValue] = useState('')
+	const { data, error } = useSWR(
+		searchValue ? `https://saavn.me/search/songs?query=${searchValue}&page=1&limit=5` : '',
+		fetcher
+	)
+
+	if (error) {
+		return <div>failed to load</div>
+	}
+
+	return (
+		<div className='flex grid-flow-row flex-col gap-y-7'>
+			<div className='flex items-center justify-between'>
+				<Button shape='circle' onClick={() => window.history.back()}>
+					<BackChevron fillClassName='fill-dark-100' />
+				</Button>
+				<span className='text-2xl font-semibold'>Search</span>
+				<Button>
+					<KeyboardIcon />
+				</Button>
+			</div>
+			<div className='relative'>
+				<span className='text-gray-500 pointer-events-none absolute inset-y-4 pl-6 sm:text-sm'>
+					<SearchIcon fillClassName='fill-dark-100' />
+				</span>
+				<form
+					onSubmit={e => {
+						e.preventDefault()
+						setSearchValue(inputRef.current?.value || '')
+					}}
+				>
+					<input
+						ref={inputRef}
+						className='bg-transparent neumorphism w-full rounded-2xl py-3 pl-14 pr-4 text-lg placeholder:text-dark-100 focus-visible:outline-0 '
+						placeholder='Search Your Music'
+						type='search'
+					/>
+				</form>
+			</div>
+			<span className='text-lg text-dark-100'>Recently Searched</span>
+			<div className='flex flex-col gap-10'>
+				{data?.results?.map((song: Song) => {
+					const { image, name, id, year, downloadUrl } = song
+					return (
+						<div
+							key={id}
+							className='flex items-center gap-3'
+							onClick={() => {
+								navigate('/player', {
+									state: {
+										image: image[2].link,
+										singer: year,
+										title: name,
+										trackSrc: downloadUrl[4]?.link
+									}
+								})
+							}}
+						>
+							<img
+								alt={name}
+								className={'row-span-2 mx-4 h-[60px] w-[60px] rotate-45 rounded-xl bg-cover'}
+								src={image?.[2]?.link}
+							/>
+							<div className='flex flex-col'>
+								<span className='text-base'>{name}</span>
+								<span className='text-xs text-dark-100'>{year}</span>
+							</div>
+						</div>
+					)
+				})}
+			</div>
+		</div>
+	)
+}
+
+export default SearchPage
