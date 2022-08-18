a0a9a8b4e66c953a10b0c7d8667b59b0cab23c5c
Mayur Panchal <mayur.panchal@deepintent.com>
2022-08-18 09:44:30 +0530

    feat: :sparkles: add not found route

--- apps/music-player/src/app/app.tsx
+++ apps/music-player/src/app/app.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import NotFound from './pages/NotFound'
 
 export const App = () => {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route element={<Home />} path='/' />
+				<Route element={<NotFound />} path='/404' />
 			</Routes>
 		</BrowserRouter>
 	)
