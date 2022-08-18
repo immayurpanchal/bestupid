9d3d729909adcf62a13f253c77ca01a7cb099b0c
Mayur Panchal <mayur.panchal@deepintent.com>
2022-08-18 09:51:39 +0530

    feat: :sparkles: add player page

--- apps/music-player/src/app/App.tsx
+++ apps/music-player/src/app/App.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Details from './pages/Details'
 import Home from './pages/Home'
 import NotFound from './pages/NotFound'
+import Player from './pages/Player'
 
 export const App = () => {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route element={<Home />} path='/' />
+				<Route element={<Player />} path='/player' />
 				<Route element={<Details />} path='/details' />
 				<Route element={<NotFound />} path='/404' />
 			</Routes>
---
+++ apps/music-player/src/app/pages/Player.tsx
@@ -0,0 +1,160 @@
+import {
+	Button,
+	BackChevron,
+	Disk,
+	Heart,
+	More,
+	Mute,
+	Next,
+	Pause,
+	Play,
+	Playlist,
+	Previous,
+	Random,
+	Repeat,
+	Volume
+} from '@bestupid/core'
+import { useEffect, useRef, useState } from 'react'
+import { useLocation } from 'react-router-dom'
+import useSWR from 'swr'
+
+import { Song } from '../types/song'
+
+export type PlayerProps = {
+	singer: string
+	title: string
+	trackSrc: string
+	image: string
+}
+
+const fetcher = (url: string) =>
+	fetch(url)
+		.then(r => r.json())
+		.then(r => r.results)
+
+const Player = () => {
+	const { state } = useLocation()
+	const { id } = state as { id: string }
+	const { data, error } = useSWR(`https://saavn.me/songs?id=${id}`, fetcher)
+	const playerRef = useRef<HTMLAudioElement>(null)
+	const [isPlaying, setIsPlaying] = useState(false)
+
+	const onPlay = () => {
+		if (playerRef.current) {
+			playerRef.current.play()
+			setIsPlaying(true)
+		}
+	}
+
+	const onPause = () => {
+		if (playerRef.current) {
+			playerRef.current.pause()
+			setIsPlaying(false)
+		}
+	}
+
+	const handlePlaylistMenu = () => {
+		console.warn('playlist menu')
+	}
+
+	const handleMore = () => {
+		console.warn('more')
+	}
+
+	const handleNext = () => {
+		console.warn('next')
+	}
+
+	const handlePrevious = () => {
+		console.warn('previous')
+	}
+
+	const handleVolumeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		if (playerRef.current) {
+			playerRef.current.volume = +Number(+e.target.value / 100).toFixed(2)
+		}
+	}
+
+	useEffect(() => {
+		if (data && !isPlaying) {
+			onPlay()
+		}
+	}, [data, isPlaying])
+
+	if (error) {
+		return <div>failed to load</div>
+	}
+
+	if (!data) {
+		return <div>Loading...</div>
+	}
+
+	const { artist: singer, name: title, downloadUrl, image: imageUrl } = data as Song
+	const trackSrc = downloadUrl?.[4]?.link
+	const image = imageUrl?.[2]?.link
+
+	return (
+		<div className='grid gap-y-16'>
+			<div className='flex items-center justify-between'>
+				<Button shape='circle' onClick={() => window.history.back()}>
+					<BackChevron fillClassName='fill-dark-100' />
+				</Button>
+				<span className='text-2xl'>Now Playing</span>
+				<div className='flex gap-x-3'>
+					<Button onClick={handlePlaylistMenu}>
+						<Playlist fillClassName='fill-dark-100' />
+					</Button>
+					<Button className='h-6 w-6' onClick={handleMore}>
+						<More fillClassName='fill-dark-100' />
+					</Button>
+				</div>
+			</div>
+			<div className='justify-self-center'>
+				<div className='relative'>
+					<Disk image={image || ''} />
+					<audio ref={playerRef} src={trackSrc} />
+				</div>
+			</div>
+			<div className='flex flex-col text-center'>
+				<span className='text-xl'>{title}</span>
+				<span className='text-xs'>{singer}</span>
+			</div>
+			<div className='flex items-center justify-center gap-x-4'>
+				<Button shape='circle' onClick={handlePrevious}>
+					<Previous fillClassName='fill-dark-100' />
+				</Button>
+				{!isPlaying && (
+					<Button shape='circle' size='large' onClick={onPlay}>
+						<Play fillClassName='fill-dark-100' />
+					</Button>
+				)}
+				{isPlaying && (
+					<Button action='pressed' shape='circle' size='large' onClick={onPause}>
+						<Pause fillClassName='fill-dark-900' />
+					</Button>
+				)}
+				<Button shape='circle' onClick={handleNext}>
+					<Next fillClassName='fill-dark-100' />
+				</Button>
+			</div>
+			<div className='flex items-center justify-between gap-x-3'>
+				<Mute className='h-6 w-6' fillClassName='fill-dark-100' />
+				<input id='volume' name='volume' type='range' onChange={handleVolumeChange} />
+				<Volume className='h-6 w-6' fillClassName='fill-dark-100' />
+			</div>
+			<div className='flex justify-between'>
+				<Button shape='circle'>
+					<Heart />
+				</Button>
+				<Button shape='circle'>
+					<Random />
+				</Button>
+				<Button shape='circle'>
+					<Repeat />
+				</Button>
+			</div>
+		</div>
+	)
+}
+
+export default Player
